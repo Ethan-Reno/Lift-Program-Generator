9c1f41cee3477d7e5cbaa01fe85014be209a8a31
Ethan Reno <epreno@live.ca>
2021-04-05 11:21:51 -0400

    Add login components

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import theme from './theme.js'
+import { ThemeProvider } from '@material-ui/core/styles';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>,
   document.getElementById('root')
 );
 
---
+++ client/src/theme.js
@@ -0,0 +1,12 @@
+import red from '@material-ui/core/colors/red';
+import grey from '@material-ui/core/colors/grey';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+// A custom theme for this app
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+});
+
+export default theme;
