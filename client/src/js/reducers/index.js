0853df623c5272b219dda39ff2c004d4605fb6aa
Ethan Reno <epreno@live.ca>
2021-04-05 19:28:39 -0400

    Redux test

--- client/src/js/reducers/index.js
+++ client/src/js/reducers/index.js
@@ -6,12 +6,25 @@
 // Payload describes what should change and can be ommited if no data to save
 // Best practice is to wrap every action within a function
 
+// When an action is dispatched, the store forwards a message (the action object) to the reducer
+// The reducer produces the next state, eventually merging the action payload into the new state
+
+import { ADD_ARTICLE } from "../constants/action-types";
+
 const initialState = {
   articles: []
 };
 
+
+// avoiding mutations in Redux:
+  // use concat, slice, or the spread operator for arrays
+  // use Object.assign or object spread of objects
 function rootReducer(state = initialState, action) {
-  // initial state is passed as a default parameter
+  if (action.type === ADD_ARTICLE) {
+    return Object.assign({}, state, {
+      articles: state.articles.concat(action.payload)
+    });
+  }
   return state;
 };
 
