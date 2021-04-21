136e3813e8db98e1ab1821b6e864b8eeeb88d3b6
Ethan Reno <epreno@live.ca>
2021-04-21 14:19:49 -0400

    Add cycle pagination

--- client/src/programs/display/program-details.component.tsx
+++ client/src/programs/display/program-details.component.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
+import { Table, TableBody, TableCell, TableContainer } from '@material-ui/core';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Grid from '@material-ui/core/Grid';
@@ -11,9 +8,12 @@ import Paper from '@material-ui/core/Paper';
 import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import ArrowLeft from '@material-ui/icons/ArrowLeft';
+import ArrowRight from '@material-ui/icons/ArrowRight';
 import { useSelector } from 'react-redux';
-import { ProgramInputs } from "../program.types";
-import { sessions } from '../program.lifts';
+import { useState } from 'react';
+import { Program, Lift } from "../program.types";
 
 const useStyles = makeStyles((theme) => ({
   heroContent: {
@@ -28,28 +28,32 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function Program(props) {
-  
+export default function ProgramDisplay(props) {
   const classes = useStyles();
   // const history = useHistory();
   const programs = useSelector((state: any) => state.programs.programs)
+  const [currentCycleIndex, setCurrentCycleIndex] = useState(0);
   
   // set currentProgram to the program with a uuid matching the url uuid parameter
   let currentProgram;
   const id = props.match.params.id
-  programs.forEach((program: ProgramInputs) => {
+  programs.forEach((program: Program) => {
     if (":" + program.uuid === id) {
       currentProgram = program;
     }
   })
 
-  let currentCycle = currentProgram.cycles[0];
-  // console.log(currentCycle.lifts);
+  let currentCycle = currentProgram.cycles[currentCycleIndex];
+  let lifts = currentCycle.lifts;
+
+  const isCycleAtZero: boolean = currentCycleIndex === 0;
+  const isCycleAtMax: boolean = currentCycleIndex === currentProgram.cycles.length - 1;
 
   return (
     <React.Fragment>
       <CssBaseline />
       <main>
+
         <div className={classes.heroContent}>
           <Container maxWidth="sm">
             <Typography component="h1" variant="h3" align="center" color="textPrimary" gutterBottom>
@@ -58,11 +62,23 @@ export default function Program(props) {
           </Container>
         </div>
 
-        {/* {activeLifts.map((lift) => (
-          <Container className={classes.cardGrid}>
+        <Grid container direction="row" justify="center" alignItems="center">
+          {
+          isCycleAtZero? <Typography></Typography> 
+          : <IconButton onClick={() => setCurrentCycleIndex(currentCycleIndex - 1)}> <ArrowLeft /> </IconButton>
+          }
+          <Typography>Cycle: {currentCycleIndex + 1}</Typography>
+          {
+          isCycleAtMax? <Typography></Typography>
+          : <IconButton onClick={() => setCurrentCycleIndex(currentCycleIndex + 1)}><ArrowRight /></IconButton>
+          }
+        </Grid>
+
+        {lifts.map((lift: Lift) => (
+          <Container className={classes.cardGrid} key={lift.name}>
             <Typography>{lift.name}</Typography>
             <Grid container spacing={2}>
-              {sessions.map((session) => (
+              {lift.sessions.map((session) => (
                 <Grid item lg={3}>
                   <TableContainer component={Paper}>
                     <Table aria-label="simple table">
@@ -75,39 +91,22 @@ export default function Program(props) {
                       </TableHead>
 
                       <TableBody>
+                        {session.sets.map((set) => (
                           <TableRow>
-                            <TableCell align="justify">{lift.oneRepMax * session.setValues.one}</TableCell>
-                            <TableCell align="justify">{session.repValues.one}</TableCell>
+                            <TableCell align="justify">{set.weight}</TableCell>
+                            <TableCell align="justify">{set.reps}</TableCell>
                           </TableRow>
-                          <TableRow>
-                            <TableCell align="justify">{lift.oneRepMax * session.setValues.two}</TableCell>
-                            <TableCell align="justify">{session.repValues.two}</TableCell>
-                          </TableRow>
-                          <TableRow>
-                            <TableCell align="justify">{lift.oneRepMax * session.setValues.three}</TableCell>
-                            <TableCell align="justify">{session.repValues.three}</TableCell>
-                          </TableRow>
-                          <TableRow>
-                            <TableCell align="justify">{lift.oneRepMax * session.setValues.four}</TableCell>
-                            <TableCell align="justify">{session.repValues.four}</TableCell>
-                          </TableRow>
-                          <TableRow>
-                            <TableCell align="justify">{lift.oneRepMax * session.setValues.five}</TableCell>
-                            <TableCell align="justify">{session.repValues.five}</TableCell>
-                          </TableRow>
-                          <TableRow>
-                            <TableCell align="justify">{lift.oneRepMax * session.setValues.six}</TableCell>
-                            <TableCell align="justify">{session.repValues.six}</TableCell>
-                          </TableRow>
+                        ))}
                       </TableBody>
-
+                      
                     </Table>
                   </TableContainer>
                 </Grid>
               ))}
             </Grid>
           </Container>
-        ))} */}
+        ))}
+
       </main>
     </React.Fragment>
   );
