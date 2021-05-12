e8829c7b02766d4afb1f5efab3697b8001fa912c
Ethan Reno <epreno@live.ca>
2021-05-12 15:59:01 -0400

    Basic CRUD operations on mock model

--- server/routes/posts.ts
+++ server/routes/posts.ts
@@ -1,28 +1,63 @@
+
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post.ts');
 
-router.get('/', (req, res) => {
-  res.send('We are on posts')
+// Get all posts
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.json({message: err})
+  }
 });
 
-router.get('/specific', (req, res) => {
-  res.send('Specific post');
-});
+// Get specific post
+router.get('/:postId', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
+  } catch (err) {
+    res.sjon({message: err});
+  };
+})
 
-router.post('/', (req, res) => {
+// Create a post
+router.post('/', async (req, res) => {
   const post = new Post({
     title: req.body.title,
     description: req.body.description
   });
-
-  post.save()
-  .then(data => {
-    res.status(200).json(data);
-  })
-  .catch(err => {
+  try{
+  const savedPost = await post.save();
+  res.json(savedPost)
+  } catch (err) {
     res.json({message: err})
-  })
+  };
+})
+
+//Update post
+router.patch('/:postId', async (req, res) => {
+  try {
+    const updatedPost = await Post.updateOne(
+      {_id: req.params.postId},
+      {$set: {title: req.body.title}}
+    );
+    res.json(updatedPost);
+  } catch (err) {
+    res.json({message: err})
+  };
+})
+
+//Delete post
+router.delete('/:postId', async (req, res) => {
+  try {
+    const post = await Post.deleteOne({_id: req.params.postId})
+    res.json(post);
+  } catch (err) {
+    res.json({message: err})
+  };
 })
 
 module.exports = router;
