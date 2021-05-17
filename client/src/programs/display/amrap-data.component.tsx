ab324ecec35869c7e44c00bd5272a46fe0d403c1
Ethan Reno <epreno@live.ca>
2021-05-17 09:23:45 -0400

    Psuedo for amrap data

--- client/src/programs/display/amrap-data.component.tsx
+++ client/src/programs/display/amrap-data.component.tsx
@@ -3,6 +3,14 @@ import {Typography} from '@material-ui/core';
 
 export default function AmrapData () {
 
+// bring in amrap data
+// create an MUI sortable table
+// forEach amrap data point, create a cell
+// include lift info in the cell
+// include a delete button in the cell
+  // delete button will be a redux action to delete the amrap data point
+  // include a confirmation alert - delete is permanent
+
   return (
     <Typography>Test</Typography>
 
